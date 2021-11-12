172caacf7e6b5f0600da98e228b3d66453acb302
Thang Le Quoc <thanglequoc.it@gmail.com>
2021-11-12 08:56:03 +0700

    Add script for SQLServer

--- sqlserver/CreateTables_vn_units.sql
+++ sqlserver/CreateTables_vn_units.sql
@@ -2,10 +2,10 @@
 -- CREATE administrative_regions TABLE
 CREATE TABLE administrative_regions (
 	id integer NOT NULL,
-	name varchar(255) NOT NULL,
-	name_en varchar(255) NOT NULL,
-	code_name varchar(255) NULL,
-	code_name_en varchar(255) NULL,
+	name nvarchar(255) NOT NULL,
+	name_en nvarchar(255) NOT NULL,
+	code_name nvarchar(255) NULL,
+	code_name_en nvarchar(255) NULL,
 	CONSTRAINT administrative_regions_pkey PRIMARY KEY (id)
 );
 
@@ -13,24 +13,24 @@ CREATE TABLE administrative_regions (
 -- CREATE administrative_units TABLE
 CREATE TABLE administrative_units (
 	id integer NOT NULL,
-	full_name varchar(255) NULL,
-	full_name_en varchar(255) NULL,
-	short_name varchar(255) NULL,
-	short_name_en varchar(255) NULL,
-	code_name varchar(255) NULL,
-	code_name_en varchar(255) NULL,
+	full_name nvarchar(255) NULL,
+	full_name_en nvarchar(255) NULL,
+	short_name nvarchar(255) NULL,
+	short_name_en nvarchar(255) NULL,
+	code_name nvarchar(255) NULL,
+	code_name_en nvarchar(255) NULL,
 	CONSTRAINT administrative_units_pkey PRIMARY KEY (id)
 );
 
 
 -- CREATE provinces TABLE
 CREATE TABLE provinces (
-	code varchar(20) NOT NULL,
-	name varchar(255) NOT NULL,
-	name_en varchar(255) NULL,
-	full_name varchar(255) NOT NULL,
-	full_name_en varchar(255) NULL,
-	code_name varchar(255) NULL,
+	code nvarchar(20) NOT NULL,
+	name nvarchar(255) NOT NULL,
+	name_en nvarchar(255) NULL,
+	full_name nvarchar(255) NOT NULL,
+	full_name_en nvarchar(255) NULL,
+	code_name nvarchar(255) NULL,
 	administrative_unit_id integer NULL,
 	administrative_region_id integer NULL,
 	CONSTRAINT provinces_pkey PRIMARY KEY (code)
@@ -45,13 +45,13 @@ ALTER TABLE provinces ADD CONSTRAINT provinces_administrative_unit_id_fkey FOREI
 
 -- CREATE districts TABLE
 CREATE TABLE districts (
-	code varchar(20) NOT NULL,
-	name varchar(255) NOT NULL,
-	name_en varchar(255) NULL,
-	full_name varchar(255) NULL,
-	full_name_en varchar(255) NULL,
-	code_name varchar(255) NULL,
-	province_code varchar(20) NULL,
+	code nvarchar(20) NOT NULL,
+	name nvarchar(255) NOT NULL,
+	name_en nvarchar(255) NULL,
+	full_name nvarchar(255) NULL,
+	full_name_en nvarchar(255) NULL,
+	code_name nvarchar(255) NULL,
+	province_code nvarchar(20) NULL,
 	administrative_unit_id integer NULL,
 	CONSTRAINT districts_pkey PRIMARY KEY (code)
 );
@@ -66,13 +66,13 @@ ALTER TABLE districts ADD CONSTRAINT districts_province_code_fkey FOREIGN KEY (p
 
 -- CREATE wards TABLE
 CREATE TABLE wards (
-	code varchar(20) NOT NULL,
-	name varchar(255) NOT NULL,
-	name_en varchar(255) NULL,
-	full_name varchar(255) NULL,
-	full_name_en varchar(255) NULL,
-	code_name varchar(255) NULL,
-	district_code varchar(20) NULL,
+	code nvarchar(20) NOT NULL,
+	name nvarchar(255) NOT NULL,
+	name_en nvarchar(255) NULL,
+	full_name nvarchar(255) NULL,
+	full_name_en nvarchar(255) NULL,
+	code_name nvarchar(255) NULL,
+	district_code nvarchar(20) NULL,
 	administrative_unit_id integer NULL,
 	CONSTRAINT wards_pkey PRIMARY KEY (code)
 );
