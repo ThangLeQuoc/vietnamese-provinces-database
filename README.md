ba5a94ad045d0763cb74758de900e4fe7474544a
Thang Le Quoc <thanglequoc.it@gmail.com>
2021-11-14 18:05:30 +0700

    Add guide for Oracle

--- README.md
+++ README.md
@@ -4,9 +4,21 @@ A complete SQL database of Vietnamese administrative units, includes all 63 Viet
 
 ## Overview
 
+The author(s) of this repository is not associated with the General Statistics Office of Vietnam, nor the Vietnamese government.
+
+### Additional change make by this repository
+
+
+- Add `administrative_regions` table with data
+- Add `administrative_units` table with data
+- Define the administrative unit and associated region for province, district and ward data
+- Define the English name
+
+
 ## Database Structure
 
-### 
+
+###  
 
 ## Installation
 
@@ -49,7 +61,12 @@ Execute the `CreateTable_vn_units.sql` in the [sqlserver directory](sqlserver) f
 Then follow up by executing the `ImportData_vn_units.sql` to import data to these generated tables.
 
 ### Oracle
-(Coming soon)
+
+Either use your existing database, or create a new one
+
+Execute the `CreateTable_vn_units.sql` in the [oracle directory](oracle) first in the target database to generate all the table structure.
+
+Then follow up by executing the `ImportData_vn_units.sql` to import data to these generated tables.
 
 ## Sample Query
 
---
+++ oracle/CreateTables_vn_units.sql
@@ -0,0 +1,84 @@
+
+-- CREATE administrative_regions TABLE
+CREATE TABLE administrative_regions (
+	id integer NOT NULL,
+	name varchar(255) NOT NULL,
+	name_en varchar(255) NOT NULL,
+	code_name varchar(255) NULL,
+	code_name_en varchar(255) NULL,
+	CONSTRAINT administrative_regions_pkey PRIMARY KEY (id)
+);
+
+
+-- CREATE administrative_units TABLE
+CREATE TABLE administrative_units (
+	id integer NOT NULL,
+	full_name varchar(255) NULL,
+	full_name_en varchar(255) NULL,
+	short_name varchar(255) NULL,
+	short_name_en varchar(255) NULL,
+	code_name varchar(255) NULL,
+	code_name_en varchar(255) NULL,
+	CONSTRAINT administrative_units_pkey PRIMARY KEY (id)
+);
+
+
+-- CREATE provinces TABLE
+CREATE TABLE provinces (
+	code varchar(20) NOT NULL,
+	name varchar(255) NOT NULL,
+	name_en varchar(255) NULL,
+	full_name varchar(255) NOT NULL,
+	full_name_en varchar(255) NULL,
+	code_name varchar(255) NULL,
+	administrative_unit_id integer NULL,
+	administrative_region_id integer NULL,
+	CONSTRAINT provinces_pkey PRIMARY KEY (code)
+);
+
+
+-- provinces foreign keys
+
+ALTER TABLE provinces ADD CONSTRAINT provinces_administrative_region_id_fkey FOREIGN KEY (administrative_region_id) REFERENCES administrative_regions(id);
+ALTER TABLE provinces ADD CONSTRAINT provinces_administrative_unit_id_fkey FOREIGN KEY (administrative_unit_id) REFERENCES administrative_units(id);
+
+
+-- CREATE districts TABLE
+CREATE TABLE districts (
+	code varchar(20) NOT NULL,
+	name varchar(255) NOT NULL,
+	name_en varchar(255) NULL,
+	full_name varchar(255) NULL,
+	full_name_en varchar(255) NULL,
+	code_name varchar(255) NULL,
+	province_code varchar(20) NULL,
+	administrative_unit_id integer NULL,
+	CONSTRAINT districts_pkey PRIMARY KEY (code)
+);
+
+
+-- districts foreign keys
+
+ALTER TABLE districts ADD CONSTRAINT districts_administrative_unit_id_fkey FOREIGN KEY (administrative_unit_id) REFERENCES administrative_units(id);
+ALTER TABLE districts ADD CONSTRAINT districts_province_code_fkey FOREIGN KEY (province_code) REFERENCES provinces(code);
+
+
+
+-- CREATE wards TABLE
+CREATE TABLE wards (
+	code varchar(20) NOT NULL,
+	name varchar(255) NOT NULL,
+	name_en varchar(255) NULL,
+	full_name varchar(255) NULL,
+	full_name_en varchar(255) NULL,
+	code_name varchar(255) NULL,
+	district_code varchar(20) NULL,
+	administrative_unit_id integer NULL,
+	CONSTRAINT wards_pkey PRIMARY KEY (code)
+);
+
+
+-- wards foreign keys
+
+ALTER TABLE wards ADD CONSTRAINT wards_administrative_unit_id_fkey FOREIGN KEY (administrative_unit_id) REFERENCES administrative_units(id);
+ALTER TABLE wards ADD CONSTRAINT wards_district_code_fkey FOREIGN KEY (district_code) REFERENCES districts(code);
