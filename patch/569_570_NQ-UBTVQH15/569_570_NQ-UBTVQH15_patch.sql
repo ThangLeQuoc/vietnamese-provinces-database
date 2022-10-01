8093ddd95ca93c3769314364afa1721dfd1df4ed
tle <tle@tripadvisor.com>
2022-10-01 20:28:04 +0700

    Remove empty line

--- patch/569_570_NQ-UBTVQH15/569_570_NQ-UBTVQH15_patch.sql
+++ patch/569_570_NQ-UBTVQH15/569_570_NQ-UBTVQH15_patch.sql
@@ -24,5 +24,3 @@ WHERE code IN ('25432');
 UPDATE wards
 SET full_name = CONCAT('Phường ', name), full_name_en  = CONCAT(name_en, ' Ward'), administrative_unit_id = 8
 WHERE code IN ('25433','25441','25444','25447');
-
-
