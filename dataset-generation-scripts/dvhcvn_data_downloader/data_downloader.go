129f49989cc5b0c09260faced8165445fb723288
Thang Le Quoc <thanglequoc.it@gmail.com>
2023-05-13 20:16:06 +0700

    Convert response to dvhcvn model

--- dataset-generation-scripts/dvhcvn_data_downloader/data_downloader.go
+++ dataset-generation-scripts/dvhcvn_data_downloader/data_downloader.go
@@ -1,10 +1,10 @@
-package dvchvn_data_downloader
+package dvhcvn_data_downloader
 
 import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	// "regexp"
+	"regexp"
 	"strings"
 
 	"github.com/dlclark/regexp2"
@@ -18,7 +18,7 @@ func GetData() {
 	<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
 		<soap:Body>
 			<DanhMucPhuongXa xmlns="http://tempuri.org/">
-				<DenNgay>30/05/2001</DenNgay>
+				<DenNgay>30/05/2023</DenNgay>
 			</DanhMucPhuongXa>
 		</soap:Body>
 	</soap:Envelope>
@@ -31,6 +31,8 @@ func GetData() {
 
 	body, err := ioutil.ReadAll(res.Body)
 	bodyString := string(body)
+
+
 	// fmt.Println(string(bodyString))
 	findAllMatch(bodyString)
 }
@@ -39,8 +41,10 @@ func findAllMatch(res string) {
 	regexPattern := regexp2.MustCompile(`(?<=<TABLE\b[^>]*>)([\s\S\n]*?)(?=<\/TABLE>)`, 0)
 
 	result := regexp2FindAllString(regexPattern, res)
+
 	for _, res := range result {
 		fmt.Println(res)
+		fmt.Println(toDvhcvnModel(res))
 	}
 }
 
@@ -52,4 +56,60 @@ func regexp2FindAllString(re *regexp2.Regexp, s string) []string {
 		m, _ = re.FindNextMatch(m)
 	}
 	return matches
+}
+
+/*
+Create the DvhcvnModel from the dvhcvn response object.
+Input should be the dvhcvn content within the <TABLE> tag
+E.g: <MaTinh>68</MaTinh><TenTinh>Tỉnh Lâm Đồng</TenTinh><MaQuanHuyen>675</MaQuanHuyen><TenQuanHuyen>Huyện Lạc Dương</TenQuanHuyen><MaPhuongXa>24846</MaPhuongXa><TenPhuongXa>Thị trấn Lạc Dương</TenPhuongXa><LoaiHinh>Thị trấn</LoaiHinh>
+*/
+func toDvhcvnModel(s string) DvhcvnModel {
+	maTinhRegex := regexp.MustCompile("<MaTinh>(.+)<\\/MaTinh>")
+	maTinh := sanitizeString(maTinhRegex.FindStringSubmatch(s)[1])
+
+	tenTinhRegex := regexp.MustCompile("<TenTinh>(.+)<\\/TenTinh>")
+	tenTinh := sanitizeString(tenTinhRegex.FindStringSubmatch(s)[1])
+
+	maQuanHuyenRegex := regexp.MustCompile("<MaQuanHuyen>(.+)<\\/MaQuanHuyen>")
+	maQuanHuyen := sanitizeString(maQuanHuyenRegex.FindStringSubmatch(s)[1])
+
+	tenQuanHuyenRegex := regexp.MustCompile("<TenQuanHuyen>(.+)<\\/TenQuanHuyen>")
+	tenQuanHuyen := sanitizeString(tenQuanHuyenRegex.FindStringSubmatch(s)[1])
+
+	maPhuongXaRegex := regexp.MustCompile("<MaPhuongXa>(.+)<\\/MaPhuongXa>")
+	maPhuongXa := sanitizeString(maPhuongXaRegex.FindStringSubmatch(s)[1])
+
+	tenPhuongXaRegex := regexp.MustCompile("<TenPhuongXa>(.+)<\\/TenPhuongXa>")
+	tenPhuongXa := sanitizeString(tenPhuongXaRegex.FindStringSubmatch(s)[1])
+
+	loaiHinhRegex := regexp.MustCompile("<LoaiHinh>(.+)<\\/LoaiHinh>")
+	loaiHinh := sanitizeString(loaiHinhRegex.FindStringSubmatch(s)[1])
+
+	return DvhcvnModel {
+		MaTinh: maTinh,
+		TenTinh: convertStandardUnitName(tenTinh),
+		MaQuanHuyen: maQuanHuyen,
+		TenQuanHuyen: convertStandardUnitName(tenQuanHuyen),
+		MaPhuongXa: maPhuongXa,
+		TenPhuongXa: convertStandardUnitName(tenPhuongXa),
+		LoaiHinh: loaiHinh,
+	}
+}
+
+func sanitizeString(s string) string {
+	return strings.Trim(
+		strings.ReplaceAll(s, "  ", " "), " ")
+}
+
+func convertStandardUnitName(s string) string {
+	if (strings.HasPrefix(s, "Thành Phố")) {
+		return strings.Replace(s, "Thành Phố", "Thành phố", 1)
+	}
+	if (strings.HasPrefix(s, "Thị Xã")) {
+		return strings.Replace(s, "Thị Xã", "Thị xã", 1)
+	}
+	if (strings.HasPrefix(s, "Thị Trấn")) {
+		return strings.Replace(s, "Thị Trấn", "Thị trấn", 1)
+	}
+	return s
 }
---
+++ dataset-generation-scripts/dvhcvn_data_downloader/dvhcvn_model.go
@@ -0,0 +1,11 @@
+package dvhcvn_data_downloader
+
+type DvhcvnModel struct {
+	MaTinh string
+	TenTinh string
+	MaQuanHuyen string
+	TenQuanHuyen string
+	MaPhuongXa string
+	TenPhuongXa string
+	LoaiHinh string
+}
