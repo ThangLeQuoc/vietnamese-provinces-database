9a3df01e85e4a78b627b830afafb83f847ab5eb9
Thang Le Quoc <thanglequoc.it@gmail.com>
2024-07-27 11:25:16 +0700

    WIP

--- dataset-generation-scripts/gis/gis_data_reader.go
+++ dataset-generation-scripts/gis/gis_data_reader.go
@@ -2,6 +2,23 @@
 
 package gis
 
-func readGISDataFiles() {
-	
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+func ReadGISDataFiles() ProvinceGIS {
+	jsonFile, err := os.Open("./resources/gis/01.json")
+	if err != nil {
+    fmt.Println(err)
+	}
+
+
+	var dvhcvnGIS ProvinceGIS
+	byteValue, _ := io.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &dvhcvnGIS)
+
+	return dvhcvnGIS
 }
--- dataset-generation-scripts/gis/gis_model.go
+++ dataset-generation-scripts/gis/gis_model.go
@@ -1,5 +1,9 @@
 package gis
 
+import (
+	"encoding/json"
+)
+
 type LatLng struct {
 	Latitude float64
 	Longitude float64
@@ -8,14 +12,18 @@ type LatLng struct {
 type ProvinceGIS struct {
 	LevelId string `json:"level1_id"`
 	Name string `json:"name"`
-
 	Districts []DistrictGIS `json:"level2s"`
+	Coordinates [][][][]float64 `json:"coordinates"`
 	BBox BBox `json:"bbox"`
 	Type string `json:"type"`
 }
 
 type DistrictGIS struct {
-
+	LevelId string `json:"level2_id"`
+	Name string `json:"name"`
+	Coordinates [][][][]float64 `json:"coordinates"`
+	BBox BBox `json:"bbox"`
+	Type string `json:"type"`
 }
 
 type BBox struct {
@@ -26,12 +34,15 @@ type BBox struct {
 }
 
 
+func (b *BBox) UnmarshalJSON(data []byte) error {
+	var bboxArray [4]float64
+	if err := json.Unmarshal(data, &bboxArray); err != nil {
+			return err
+	}
 
-/*
-- level_id
-- name
-- coordinates
-- bbox
-- type
-
-*/
+	b.TopLeft = bboxArray[0]
+	b.TopRight = bboxArray[1]
+	b.BottomLeft = bboxArray[2]
+	b.BottomRight = bboxArray[3]
+	return nil
+}
