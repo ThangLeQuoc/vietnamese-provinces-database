3f4cf86291e95c72911aa0b6f117375e112a6e22
Thang Le Quoc <thanglequoc.it@gmail.com>
2024-11-09 14:30:05 +0700

    Update data patch with until Decree 1203

--- dataset-generation-scripts/dvhcvn_data_downloader/data_downloader.go
+++ dataset-generation-scripts/dvhcvn_data_downloader/data_downloader.go
@@ -3,10 +3,11 @@ package dvhcvn_data_downloader
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"slices"
 	"strings"
 	"time"
-	"log"
 )
 
 const DVHCVN_URL = "https://danhmuchanhchinh.gso.gov.vn/DMDVHC.asmx"
@@ -19,24 +20,44 @@ func FetchDvhcvnData(publicDataDate time.Time) []DvhcvnModel {
 	fmt.Printf("⬇️ Downloading provinces data patch from %s\n", DVHCVN_URL)
 	dataAPIDateStr := publicDataDate.Format("02/01/2006") // dd/MM/YYYY
 
-	httpRequestBody := fmt.Sprintf(`<?xml version="1.0" encoding="utf-8"?>
+	// Fetch ward-level data
+	wardRequestBody := createSoapRequest("DanhMucPhuongXa", dataAPIDateStr)
+	wardData := fetchDataFromAPI(wardRequestBody)
+	result := extractDvhcvnUnits(wardData)
+
+	// Fetch district-level data
+	districtRequestBody := createSoapRequest("DanhMucQuanHuyen", dataAPIDateStr)
+	districtData := fetchDataFromAPI(districtRequestBody)
+	districtResult := extractDvhcvnUnits(districtData)
+
+	// Concatenate and return results
+	return slices.Concat(result, districtResult)
+}
+
+// Helper function to create a SOAP request body
+func createSoapRequest(action, date string) string {
+	return fmt.Sprintf(`<?xml version="1.0" encoding="utf-8"?>
 	<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
 		<soap:Body>
-			<DanhMucPhuongXa xmlns="http://tempuri.org/">
+			<%s xmlns="http://tempuri.org/">
 				<DenNgay>%s</DenNgay>
-			</DanhMucPhuongXa>
+			</%s>
 		</soap:Body>
-	</soap:Envelope>
-	`, dataAPIDateStr)
+	</soap:Envelope>`, action, date, action)
+}
 
-	res, err := http.Post(DVHCVN_URL, "text/xml", strings.NewReader(httpRequestBody))
+// Helper function to send HTTP POST requests and fetch data
+func fetchDataFromAPI(requestBody string) string {
+	res, err := http.Post(DVHCVN_URL, "text/xml", strings.NewReader(requestBody))
 	if err != nil {
-		fmt.Println("Exception occurred while making the request")
-		log.Fatal(err)
+		log.Fatalf("Exception occurred while making the request: %v", err)
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalf("Exception occurred while reading the response: %v", err)
 	}
 
-	body, _ := io.ReadAll(res.Body)
-	bodyString := string(body)
-
-	return extractDvhcvnUnits(bodyString)
+	return string(body)
 }
